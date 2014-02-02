49d0e836ed19724e3124e1c2c92de60e4b3a8154
VanceVan <vaughn@radsoft.com>
2014-02-01 22:47:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcstealer::Application.config.secret_key_base = '0946de0108c64ccd5074f845e45ac3bebea86cac434b3b6922c4eab9a9ec6e45c2e533a8976f39183dd2eddb60a0a07790abd202ffbfbb9eff052c54f8332258'
