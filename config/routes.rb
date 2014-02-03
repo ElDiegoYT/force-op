75ba4145e10e10542c3def298477fce0e087543d
VanceVan <vaughn@radsoft.com>
2014-02-02 20:49:06 -0800

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Mcforceop::Application.routes.draw do
   
   resources :logins
+  root :to => "logins#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
