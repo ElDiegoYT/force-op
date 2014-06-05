212509781bd3f4830483cc785995a2ebddb741c7
VanceVan <vaughn@radsoft.com>
2014-06-04 23:50:48 -0700

    Some minor updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Mcforceop::Application.routes.draw do
   
   resources :logins
   root :to => "logins#index"
+  
+  get 'logins/updates' => 'logins#updates'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
